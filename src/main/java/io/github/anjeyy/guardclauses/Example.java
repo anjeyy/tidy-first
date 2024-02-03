90ae4c58edf6b342b52815f2a95232fb75cceed8
Andjelko Perisic <66070554+anjeyy@users.noreply.github.com>
2024-02-03 12:08:04 +0100

    guard clauses

--- src/main/java/io/github/anjeyy/guardclauses/Example.java
+++ src/main/java/io/github/anjeyy/guardclauses/Example.java
@@ -1,4 +1,39 @@
 package io.github.anjeyy.guardclauses;
 
 class Example {
+
+    double singleIfBefore(int x) {
+        if (x != 0) {
+            return (double) x / 2;
+        }
+        throw new UnsupportedOperationException("Division by 0.");
+    }
+
+    double singleIfAfter(int x) {
+        if (x == 0) {
+            throw new UnsupportedOperationException("Division by 0.");
+        }
+
+        return (double) x / 2;
+    }
+
+    double doubleIfBefore(int x) {
+        if (x != 0) {
+            if (x > 0) {
+                return (double) x / 2;
+            }
+            throw new UnsupportedOperationException("Division for negatives not allowed.");
+        }
+        throw new UnsupportedOperationException("Division by 0.");
+    }
+
+    double doubleIfAfter(int x) {
+        if (x == 0) {
+            throw new UnsupportedOperationException("Division by 0.");
+        }
+        if (x < 0) {
+            throw new UnsupportedOperationException("Division for negatives not allowed.");
+        }
+        return (double) x / 2;
+    }
 }
